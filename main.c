e1d714c42c4c9731002ad9a814ac70616f8ad23a
Aliou Diallo <aliou.diallo2@gmail.com>
2012-02-21 22:14:59 +0100

    Creating a boat.

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "boat.h"
+
 int main(int argc, const char * argv[]) {
-  printf("Hello, World!\n");
+
+  boat_type *fleet_player = NULL;
+  boat_type *fleet_AI = NULL;
+
+  fleet_player = createBoat();
+  fleet_AI = createBoat();
+
   return EXIT_SUCCESS;
 }
