b9b9bd8fa10243f9e817ac0d81c5a1409728f8f9
Aliou Diallo <aliou.diallo2@gmail.com>
2012-02-22 09:08:13 +0100

    Creation of the grids.
    Two grids per player : On one grid the player arranges ships and
    records the shots by the opponent. On the other grid the player records
    his own shots.

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #include "boat.h"
 
+#define GRID_SIZE 10
+
 int main(int argc, const char * argv[]) {
 
   boat_type *fleet_player = NULL;
@@ -11,5 +13,19 @@ int main(int argc, const char * argv[]) {
   fleet_player = createBoat();
   fleet_AI = createBoat();
 
+  char grid_player[GRID_SIZE][GRID_SIZE];
+  char grid_boat_player[GRID_SIZE][GRID_SIZE];
+
+  char grid_AI[GRID_SIZE][GRID_SIZE];
+  char grid_boat_AI[GRID_SIZE][GRID_SIZE];
+
+  for (i = 0 ; i < GRID_SIZE ; i++) {
+    for (j = 0; j < GRID_SIZE; j++) {
+      grid_player[i][j] = '~';
+      grid_boat_player[i][j] = '~';
+      grid_AI[i][j] = '~';
+      grid_boat_AI[i][j] = '~';
+    }
+  }
   return EXIT_SUCCESS;
 }
